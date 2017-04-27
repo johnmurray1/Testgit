9cb6f92eea3cff641ae2ede876c0118647b2cfe5
bobbyjohn.murray <john.murray@callhealth.co.in>
2017-04-27 17:33:10 +0530

    commit to second output

--- TestJava/Src/NewClass.java
+++ TestJava/Src/NewClass.java
@@ -14,6 +14,7 @@ public class NewClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hi how r u and where r u");
+		System.out.println("hi how r u and where r u");
 
 	}
 
