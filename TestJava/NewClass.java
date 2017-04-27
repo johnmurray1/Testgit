8bed3c64f3e49225ea1448cc63ce2c03b777c5bb
bobbyjohn.murray <john.murray@callhealth.co.in>
2017-04-27 17:21:22 +0530

    new class has been created

---
+++ TestJava/NewClass.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+
+/**
+ * @author bobbyjohn.murray
+ *
+ */
+public class NewClass {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hi how r u");
+
+	}
+
+}
